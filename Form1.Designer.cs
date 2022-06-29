5ddcc64ac62a6a7a8b86a01472424f42cbcf9edc
TakaRuangshawk <takaruangs@outlook.com>
2022-06-29 14:12:40 +0700

    1.0.0
    show reader, save picture, show data

---
+++ Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace testPersonalCard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.Font = new System.Drawing.Font("AngsanaUPC", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(546, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(198, 75);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CLICK ME";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("AngsanaUPC", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(28, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(471, 726);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Waiting...";
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(546, 127);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(390, 486);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(948, 773);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "testPersonCard";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ThaiNationalIDCard;
+using PCSC;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace testPersonalCard
+{
+    public partial class Form1 : Form
+    {
+      
+
+        public Form1()
+        {   
+            InitializeComponent();
+        }
+        public void LogLine(string text = "")
+        {
+            if (textBox1.InvokeRequired)
+            {
+                textBox1.BeginInvoke(new MethodInvoker(delegate { textBox1.AppendText(text + Environment.NewLine); }));
+            }
+            else
+            {
+                textBox1.AppendText(text + Environment.NewLine);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            textBox1.Text = String.Empty;
+            textBox1.Refresh();
+            LogLine("Loading...");
+            ReadCard();
+        }
+        public void ReadCard()
+        {
+
+            const string path_thaicardpic = "D:\\grgbanking\\ecat\\thainationalcard\\pic\\";
+            if (!Directory.Exists(path_thaicardpic))
+            {
+                Directory.CreateDirectory(path_thaicardpic);
+            }
+            ThaiIDCard idcard = new ThaiIDCard();
+            Personal personal = idcard.readAllPhoto();
+            textBox1.Text = String.Empty;
+            textBox1.Refresh();
+            var contextFactory = ContextFactory.Instance;
+            using (var ctx = contextFactory.Establish(SCardScope.System))
+            {
+                var readerNames = ctx.GetReaders();
+                
+                if (IsEmpty(readerNames))
+                {
+
+                    LogLine("No reader connected.");
+                    return;
+                }
+                textBox1.Text = String.Empty;
+                textBox1.Refresh();
+                var readerStates = ctx.GetReaderStatus(readerNames);
+                foreach (var state in readerStates)
+                {
+                    var atr = BitConverter.ToString(state.Atr ?? new byte[0]);
+
+                    if (state.ReaderName.Substring(0, 12) == "ACS CCID USB")
+                    {
+                        LogLine("Device Name : " + state.ReaderName);//reader name :: ACS CCID USB Reader
+                        LogLine("State Device : " + state.EventState);//event name such as present card, empty card
+                        LogLine("CurrentState : " + state.CurrentState);
+                        LogLine("CurrentStateValue : " + state.CurrentStateValue);
+                        LogLine("EventStateValue : " + state.EventStateValue);
+                        LogLine("atr : " + atr);
+                    }
+                    else
+                    {
+                        //LogLine("Device Name : " + state.ReaderName);
+                        //LogLine("State Device : " + state.EventState);
+                        //LogLine("CurrentState : " + state.CurrentState);
+                        //LogLine("CurrentStateValue : " + state.CurrentStateValue);
+                        //LogLine("EventStateValue : " + state.EventStateValue);
+                        //LogLine("atr : " + atr);
+                    }
+
+
+                    // free system resources (required for each returned state)
+                    state.Dispose();
+                }
+            }
+            if (personal != null)
+                {
+                //idcard.eventPhotoProgress += new handlePhotoProgress(photoProgress);
+                LogLine("Citizenid : " + personal.Citizenid);
+                LogLine("Birthday : " + personal.Birthday.ToString("dd/MM/yyyy"));
+                LogLine("THAI_Name : " + personal.Th_Prefix+" "+personal.Th_Firstname+" "+personal.Th_Lastname);
+                LogLine("ENG_Name : " + personal.En_Prefix + " " + personal.En_Firstname + " " + personal.En_Lastname);
+                LogLine("Issue : " + personal.Issue.ToString("dd/MM/yyyy")); // วันออกบัตร
+                LogLine("Expire : " + personal.Expire.ToString("dd/MM/yyyy")); // วันหมดอายุ
+                LogLine("Address : " + personal.Address);
+                LogLine("HouseNo : " + personal.addrHouseNo); // บ้านเลขที่
+                LogLine("VillageNo : " + personal.addrVillageNo); // หมู่ที่
+                LogLine("Lane : " + personal.addrLane); // ซอย
+                LogLine("Road : " + personal.addrRoad); // ถนน
+                LogLine("Subdistrict : " + personal.addrTambol);
+                LogLine("District : " + personal.addrAmphur);
+                LogLine("Province : " + personal.addrProvince);
+                LogLine("Issuer : " + personal.Issuer);
+                // LogLine("Photo : " + personal.PhotoBitmap.ToString());
+                //LogLine("Image : " + string.Join(",", personal.PhotoRaw));
+                //check
+                string photo = string.Join(",", personal.PhotoRaw);
+                Console.WriteLine(photo);
+                pictureBox1.Image = personal.PhotoBitmap;
+                using (Image img = Image.FromStream(new MemoryStream(personal.PhotoRaw)))
+                {
+                    string path = Path.GetFullPath(path_thaicardpic + personal.Citizenid+"_"+DateTime.Now.ToString("yyyymmdd_hhmmss")+".jpg");
+                    img.Save(path, ImageFormat.Jpeg);
+                }
+            }
+            else if (idcard.ErrorCode() > 0)
+            {
+                LogLine(idcard.Error()); // tell something when card or reader have errors.
+            }
+        }
+        #region
+        private static bool IsEmpty(ICollection<string> readerNames) =>
+          readerNames == null || readerNames.Count < 1;
+       
+        #endregion
+    }
+}
